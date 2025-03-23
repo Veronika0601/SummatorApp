336a1ef4b828a85dae0597784e6fd311e7b4935b
Veronika <veronikatodorova@abv.bg>
2025-03-23 14:35:25 +0200

    Add project files.

---
+++ SummatorApp/SummatorAppTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+
+namespace SummatorApp
+{
+    public  class SummatorAppTests 
+    {
+
+        private AndroidDriver driver;
+        private SummatorPage summatorPage;
+
+
+        [OneTimeSetUp]
+
+        public void OneTimeSetUp()
+        {
+
+            var appiumOptions = new AppiumOptions();
+
+            appiumOptions.PlatformName = "Android";
+            appiumOptions.AutomationName = "UiAutomator2";
+            appiumOptions.PlatformVersion = "14";
+            appiumOptions.DeviceName = "R5CR92WHYBT";
+            appiumOptions.App = "C:\\Users\\veron\\Desktop\\com.example.androidappsummator.apk";
+
+            driver = new AndroidDriver(appiumOptions);
+            summatorPage = new SummatorPage(driver);
+
+
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+
+        [Test]
+        public void Test_With_ValidData()
+        {
+
+            var result = summatorPage.Calculate("5", "10");
+
+            Assert.That(result, Is.EqualTo("15"));
+
+        }
+
+        [Test]
+        public void Test_With_InvalidData()
+        {
+            var result = summatorPage.Calculate("", "10");
+
+            Assert.That(result, Is.EqualTo("error"));
+        }
+
+    }
+}
---
+++ SummatorApp/SummatorPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+
+namespace SummatorApp
+{
+    public  class SummatorPage
+    {
+
+        private readonly AndroidDriver driver;
+
+      public SummatorPage(AndroidDriver driver) 
+        {
+            this.driver = driver;
+        }
+
+        public IWebElement firstField => driver.FindElement(MobileBy.Id("com.example.androidappsummator:id/editText1"));
+
+
+        public IWebElement secondField => driver.FindElement(MobileBy.Id("com.example.androidappsummator:id/editText2"));
+
+        public IWebElement calcButton => driver.FindElement(MobileBy.ClassName("android.widget.Button"));
+
+        public IWebElement result => driver.FindElement(MobileBy.XPath("//android.widget.EditText[@resource-id=\"com.example.androidappsummator:id/editTextSum\"]"));
+
+        public string Calculate(string num1 , string num2)
+        {
+            firstField.Clear();
+            firstField.SendKeys(num1);
+
+
+            secondField.Clear();
+            secondField.SendKeys(num2);
+
+
+            calcButton.Click();
+
+            return result.Text;
+        }
+
+
+    }
+}
